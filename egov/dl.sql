daa2484235940d85780621b96d5fddc4df7de709
GautamMIH <gautam.mudbhari16@gmail.com>
2023-06-25 06:16:32 +0545

    more tables in the database

--- egov/dl.sql
+++ egov/dl.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 17, 2023 at 06:08 AM
+-- Generation Time: Jun 25, 2023 at 02:30 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -24,6 +24,67 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `exam`
+--
+
+CREATE TABLE `exam` (
+  `Exid` int(11) NOT NULL,
+  `NID` int(11) DEFAULT NULL,
+  `ov` date DEFAULT NULL,
+  `we` date DEFAULT NULL,
+  `westat` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `exam`
+--
+
+INSERT INTO `exam` (`Exid`, `NID`, `ov`, `we`, `westat`) VALUES
+(1, 1, '2023-06-30', '2023-07-29', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `license`
+--
+
+CREATE TABLE `license` (
+  `DLN` int(11) NOT NULL,
+  `NID` int(11) DEFAULT NULL,
+  `DL_no` int(11) DEFAULT NULL,
+  `category` varchar(1) DEFAULT NULL,
+  `expiry` date DEFAULT NULL,
+  `felony` int(11) DEFAULT NULL,
+  `contact` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `license`
+--
+
+INSERT INTO `license` (`DLN`, `NID`, `DL_no`, `category`, `expiry`, `felony`, `contact`) VALUES
+(1, 1, 12456191, 'A', '2027-06-30', NULL, '9840480778');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trials`
+--
+
+CREATE TABLE `trials` (
+  `trid` int(11) DEFAULT NULL,
+  `ftd` date DEFAULT NULL,
+  `ftr` tinyint(1) DEFAULT NULL,
+  `sctd` date DEFAULT NULL,
+  `str` tinyint(1) DEFAULT NULL,
+  `ttd` date DEFAULT NULL,
+  `ttr` tinyint(1) DEFAULT NULL,
+  `NID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `userdata`
 --
 
@@ -38,25 +99,84 @@ CREATE TABLE `userdata` (
   `DOI` date DEFAULT NULL,
   `Gender` varchar(10) DEFAULT NULL,
   `Nationality` varchar(20) DEFAULT NULL,
-  `pwd` varchar(50) DEFAULT NULL
+  `pwd` varchar(50) DEFAULT NULL,
+  `sessionid` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `userdata`
 --
 
-INSERT INTO `userdata` (`ID`, `Name`, `Address`, `FName`, `MName`, `BGroup`, `DOB`, `DOI`, `Gender`, `Nationality`, `pwd`) VALUES
-(1, 'Gautam', 'lokanthali', 'Bhojraj', 'gayatridevi', 'A+', '2001-08-11', '2023-06-17', 'male', 'Nepali', 'Clashon124');
+INSERT INTO `userdata` (`ID`, `Name`, `Address`, `FName`, `MName`, `BGroup`, `DOB`, `DOI`, `Gender`, `Nationality`, `pwd`, `sessionid`) VALUES
+(1, 'Gautam Mudbhari', 'lokanthali', 'Bhojraj', 'gayatridevi', 'A+', '2001-08-11', '2023-06-17', 'male', 'Nepali', 'Clashon124', '649711651f0bfaefd');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `exam`
+--
+ALTER TABLE `exam`
+  ADD PRIMARY KEY (`Exid`),
+  ADD UNIQUE KEY `NID` (`NID`);
+
+--
+-- Indexes for table `license`
+--
+ALTER TABLE `license`
+  ADD PRIMARY KEY (`DLN`),
+  ADD KEY `NID` (`NID`);
+
+--
+-- Indexes for table `trials`
+--
+ALTER TABLE `trials`
+  ADD KEY `NID` (`NID`);
+
+--
 -- Indexes for table `userdata`
 --
 ALTER TABLE `userdata`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `exam`
+--
+ALTER TABLE `exam`
+  MODIFY `Exid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `license`
+--
+ALTER TABLE `license`
+  MODIFY `DLN` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `exam`
+--
+ALTER TABLE `exam`
+  ADD CONSTRAINT `exam_ibfk_1` FOREIGN KEY (`NID`) REFERENCES `userdata` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `license`
+--
+ALTER TABLE `license`
+  ADD CONSTRAINT `license_ibfk_1` FOREIGN KEY (`NID`) REFERENCES `userdata` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `trials`
+--
+ALTER TABLE `trials`
+  ADD CONSTRAINT `trials_ibfk_1` FOREIGN KEY (`NID`) REFERENCES `userdata` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
